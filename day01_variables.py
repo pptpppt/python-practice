7542daf9c16afc816374ecf69e2227edd90c0286
pptpppt <2322358814@qq.com>
2026-09-02 14:47:42 +0800

    day01 三个任务完成

--- day01_variables.py
+++ day01_variables.py
@@ -12,7 +12,7 @@ mu_chan = 800             # 亩产（斤/亩）
 zong_chan = tian_kuai_mian_ji * mu_chan
 print(f"这块 {tian_kuai_mian_ji} 亩的田，预计收 {zong_chan} 斤")
 
-name = input("请输入你的名字：")
+name = input("name：")
 print(f"你好，{name}！今天开始学 Python 了。")
 
 # ============ 任务区 ============
@@ -25,3 +25,27 @@ print(f"你好，{name}！今天开始学 Python 了。")
 
 # 任务 3（挑战）：一块 3.2 亩的田亩产 750 斤，旁边一块 1.8 亩的田亩产 900 斤，
 #         两块田的总产量是多少斤？用变量把每一步算清楚，打印出来。
+mian_ji = 3.5
+mu_chan = 750
+zong_chan = mian_ji*mu_chan
+print(f"这块{mian_ji}亩的地,预计收{zong_chan}斤")
+
+mian_ji=float(input("请输入亩数;"))
+zong_chan=mian_ji*800
+print(zong_chan)
+
+mian_ji1=3.2
+mian_ji2=1.8
+mu_chan1=750
+mu_chan2=900
+zong_mu=mian_ji1+mian_ji2
+zong_chan1=mian_ji1*mu_chan1
+zong_chan2=mian_ji2*mu_chan2
+zongchan=zong_chan1+zong_chan2
+print(f"共计{zong_mu}亩的地，总收{zongchan}斤")
+
+
+
+
+
+
